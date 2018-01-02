79cd3959a5d988936e0c3f0aa212e7ef328fe700
Apolix <apolix@gmail.hu>
2018-01-02 21:24:55 +0100

    Feltettem az alapokat amim volt

---
+++ Tower Defense/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+    public float speed = 60f;
+
+    private Transform target;
+    private int waitpointIndex = 0;
+
+    void Start()
+    {
+        target = Waitpoints.points[0];
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+
+        transform.position += dir.normalized * speed * Time.deltaTime;
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.2f)
+        {
+            GetNextWaitPoint();
+        }
+    }
+
+    void GetNextWaitPoint()
+    {
+        if (waitpointIndex >= Waitpoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        waitpointIndex++;
+        target = Waitpoints.points[waitpointIndex];
+    }
+}
---
+++ Tower Defense/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+    public Text TimeBetweenWavesText;
+
+    public float TimeBetweenWaves = 10f;
+    public int EnemyCount = 8;
+
+    private float countdown;
+
+    void Update()
+    {
+        if (countdown <= 0)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = TimeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+        TimeBetweenWavesText.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        EnemyCount = Random.Range(1, 11);
+
+        for (int i = 0; i < EnemyCount; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.4F);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
