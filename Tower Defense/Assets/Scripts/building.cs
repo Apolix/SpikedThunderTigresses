5da4852899010b2aebac65a9e233103f466589ac
SirDark <gergelykevin123@gmail.com>
2018-01-09 20:43:00 +0100

    épités alapok
    sexy vagyok

---
+++ Tower Defense/Assets/Scripts/building.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class building : MonoBehaviour {
+	public GameObject red_turret_of_death;
+	bool build_mode = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Mouse0)) {
+			build_mode = false;
+		}
+
+		if (build_mode == false) {
+			if (Input.GetKeyDown (KeyCode.Alpha1)) {
+
+				Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+				RaycastHit hit;
+
+				if (Physics.Raycast (ray, out hit)) {
+					Vector3 kerekített_pozíció = new Vector3 (Mathf.Round (hit.transform.position.x), hit.transform.position.y, Mathf.Round (hit.transform.position.z));
+					Instantiate (red_turret_of_death, kerekített_pozíció, Quaternion.identity);
+				}
+			}
+		}
+	}
+}
---
+++ Tower Defense/Assets/Scripts/placement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class placement : MonoBehaviour {
+	Ray ray, ray_tagging;
+	RaycastHit hit, hit_tagging;
+	Vector3 kerekített_pozíció;
+	public GameObject turret;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		Physics.Raycast(ray, out hit);
+		kerekített_pozíció = new Vector3 (Mathf.Round (hit.transform.position.x), 1, Mathf.Round (hit.transform.position.z));
+		transform.position = kerekített_pozíció;
+
+
+		Physics.Raycast (transform.position, Vector3.down, out hit_tagging, 10);
+		print (hit_tagging.transform.gameObject.tag);
+		if (hit_tagging.transform.gameObject.tag == "ground") {
+			if (Input.GetKeyDown(KeyCode.Mouse0)) {
+				print ("lerakva");
+				Destroy (this);
+			}
+		}
+
+
+	}
+}
