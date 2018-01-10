36236a8c33a23c48bbe37b68eaea99d5999f27e1
SirDark <gergelykevin123@gmail.com>
2018-01-10 19:16:33 +0100

    turretek lőnek
    megcsináltam hogy a turretek tudjanak lőni és hogy változzon a szinük attól függően le lehet-e rakni ahova a player le akarja (per pill a turretek mindent egyhitelnek ha lesz rendesebb gameplay ezen lehet majd változtatni)

--- Tower Defense/Assets/Scripts/placement.cs
+++ Tower Defense/Assets/Scripts/placement.cs
@@ -15,19 +15,22 @@ public class placement : MonoBehaviour {
 	void Update () {
 		ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 		Physics.Raycast(ray, out hit);
+
 		kerekített_pozíció = new Vector3 (Mathf.Round (hit.transform.position.x), 1, Mathf.Round (hit.transform.position.z));
 		transform.position = kerekített_pozíció;
 
 
 		Physics.Raycast (transform.position, Vector3.down, out hit_tagging, 10);
-		print (hit_tagging.transform.gameObject.tag);
+
 		if (hit_tagging.transform.gameObject.tag == "ground") {
+			GetComponent<Renderer> ().material.color = Color.green;
 			if (Input.GetKeyDown(KeyCode.Mouse0)) {
-				print ("lerakva");
+				GetComponent<Renderer> ().material.color = Color.black;
+				gameObject.AddComponent<shooting> ();
 				Destroy (this);
 			}
+		} else {
+			GetComponent<Renderer> ().material.color = Color.red;
 		}
-
-
 	}
 }
---
+++ Tower Defense/Assets/Scripts/shooting.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shooting : MonoBehaviour {
+	GameObject[] ellenségek;
+	public GameObject damage;
+	public float range = 15, attack_speed = 100;
+	float ticker = 0;
+	// Use this for initialization
+	void Start () {
+		print ("itt vagyok ragyogok mint a fekete szurok");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ellenségek = GameObject.FindGameObjectsWithTag ("enemy");
+		foreach (GameObject enemy in ellenségek) {
+			float távolság = Vector3.Distance (transform.position, enemy.transform.position);
+			if (távolság < range && ticker == attack_speed) {
+				shoot (enemy);
+			}
+		}
+	}
+	void FixedUpdate()
+	{
+		if (ticker < attack_speed) {
+			ticker += 1;
+		}
+		print (ticker);
+
+	}
+	void shoot(GameObject enemy_v)
+	{
+		Destroy (enemy_v);
+		ticker = 0;
+	}
+}
