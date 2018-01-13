789606c96507d2c6f506f9516c57436e7768068c
SirDark <gergelykevin123@gmail.com>
2018-01-13 12:44:57 +0100

    menü
    teljesen működik
    btw a turretek színe nekem még mindig nem változik se és nem is fordulnak rá a turretek

---
+++ Tower Defense/Assets/Scripts/menuscript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class menuscript : MonoBehaviour {
+
+	public Canvas QuitMenu;
+	public Button startgame;
+	public Button exit;
+	// Use this for initialization
+	void Start () {
+		QuitMenu = QuitMenu.GetComponent<Canvas> ();
+		startgame = startgame.GetComponent<Button> ();
+		exit = exit.GetComponent<Button> ();
+		QuitMenu.enabled = false;
+	}
+	public void ExitPress()
+	{
+		QuitMenu.enabled = true;
+		startgame.enabled = false;
+		exit.enabled = false;
+	}
+	public void nopress ()
+	{
+		QuitMenu.enabled = false;
+		startgame.enabled = true;
+		exit.enabled = true;
+	}
+	public void startpress()
+	{
+		SceneManager.LoadScene ("Level01");
+	}
+	public void ExitGame()
+	{
+		Application.Quit ();
+	}
+}
