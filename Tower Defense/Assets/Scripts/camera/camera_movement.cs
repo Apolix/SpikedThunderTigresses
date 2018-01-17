bc0e3e9751904ebc8a2ca2811a2b8a83ede86209
SirDark <gergelykevin123@gmail.com>
2018-01-17 21:17:50 +0100

    nekopara

---
+++ Tower Defense/Assets/Scripts/camera/camera_movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera_movement : MonoBehaviour {
+	public CharacterController player;
+	float moveEH, moveJB, zoom;
+	public float speed;
+	Vector3 movement;
+	// Use this for initialization
+	void Start () {
+		player.GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		moveJB = Input.GetAxis ("Horizontal") * speed;
+		moveEH = Input.GetAxis ("Vertical") * speed;
+		
+
+		Vector3 movement = new Vector3 (-moveEH, zoom, moveJB);
+		player.Move (movement * Time.deltaTime);
+	}
+}
