b8bf6a4880055ccbf37db0892585f05c9add0944
SirDark <gergelykevin123@gmail.com>
2018-01-17 22:31:47 +0100

    kamera mozgás
    magamtól csináltam ha hiányzik valami / buggos szólj

--- Tower Defense/Assets/Scripts/camera/camera_movement.cs
+++ Tower Defense/Assets/Scripts/camera/camera_movement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class camera_movement : MonoBehaviour {
 	public CharacterController player;
 	float moveEH, moveJB, zoom;
-	public float speed;
+	public float speed, scrollspeed;
 	Vector3 movement;
 	// Use this for initialization
 	void Start () {
@@ -16,7 +16,14 @@ public class camera_movement : MonoBehaviour {
 	void Update () {
 		moveJB = Input.GetAxis ("Horizontal") * speed;
 		moveEH = Input.GetAxis ("Vertical") * speed;
-		
+		zoom = Input.GetAxis ("Mouse ScrollWheel") * scrollspeed;
+
+		if (transform.position.y > 66 && zoom > 0) {
+			zoom = 0;
+		}
+		if (transform.position.y < 20 && zoom < 0) {
+			zoom = 0;
+		}
 
 		Vector3 movement = new Vector3 (-moveEH, zoom, moveJB);
 		player.Move (movement * Time.deltaTime);
